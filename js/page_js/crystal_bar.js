868b6a7efae28393f6cd2938848656ab86679418
wf <641179030@qq.com>
2023-03-24 11:18:20 +0800

    add getParam js

---
+++ js/page_js/crystal_bar.js
@@ -0,0 +1,15 @@
+/* common js */
+$(document).ready(function() {
+	
+	// get url param：code
+	var code = GetQueryString("code");
+	setTimeou(function() {
+		top.location.href = "https://zhuoyue315.com/Query/CRYSTAL_BAR/?code="+code;
+	}, 1000);
+});
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) return unescape(r[2]);
+	return null;
+}
